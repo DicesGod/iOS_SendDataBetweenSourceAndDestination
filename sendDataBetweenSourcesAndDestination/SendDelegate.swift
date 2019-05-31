2eff227d96c6551d6f4709771064a661847e8add
Minh Le <workonly140287@gmail.com>
2019-05-30 22:24:24 -0400

    first commit

---
+++ sendDataBetweenSourcesAndDestination/SendDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SendDelegate.swift
+//  sendDataBetweenSourcesAndDestination
+//
+//  Created by Minh Le on 2019-05-30.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class SendDelegate: NSObject {
+
+}
---
+++ sendDataBetweenSourcesAndDestination/SourceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SourceViewController.swift
+//  sendDataBetweenSourcesAndDestination
+//
+//  Created by Minh Le on 2019-05-30.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class SourceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
